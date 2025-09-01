c082116f439618b308c7332923dfbead78546b25
Vimal Kumar <vimal.kumar1@wipro.com>
2025-09-01 15:31:43 -0400

    state and event handlingin react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm'; 
 
 function App() {
   return (
+    <>
     <div className="App">
-      <>
+      
       <Navbar Title="textUtils"/>
-      </>
+      <div className="container my-3">
+      <TextForm heading="Enter text to analyze"/>
+      </div>
     </div>
+    </>
   );
 }
 
